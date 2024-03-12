b0024ca810328346ddfdc82b0654da78aca4cdd1
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-12 09:28:33 -0400

    added footer with link to github and linkedin. Changed document title in index.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-Weather app
+<h1>Weather app</h1>
+<footer> This project was coded by <a href="https://www.linkedin.com/in/sarah-kennedy-02b2b6293/" target="_blank" rel="noreferrer">Sarah Kennedy</a> and is <a href="https://github.com/SKennedy4123/kennedy-react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
+   </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+document.title = "React Weather App";
+
 root.render(
   <React.StrictMode>
     <App />
