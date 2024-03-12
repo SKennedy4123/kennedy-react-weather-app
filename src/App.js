ebf52efc96b02a8c4c4bae5bbe960677e7c50e37
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-12 09:51:33 -0400

    install boostrap, add html to Weather.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-<h1>Weather app</h1>
+      <div className="container">
+<Weather />
+</div>
 <footer> This project was coded by <a href="https://www.linkedin.com/in/sarah-kennedy-02b2b6293/" target="_blank" rel="noreferrer">Sarah Kennedy</a> and is <a href="https://github.com/SKennedy4123/kennedy-react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
    </footer>
     </div>
