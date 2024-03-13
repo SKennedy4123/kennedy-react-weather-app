b60e4335d10875eae1e0be36d3e5f724390d4623
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-13 09:30:34 -0400

    added weather forecast layout and api for one day

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <div className="container">
 <Weather defaultCity="New York"/>
 </div>
-<footer> This project was coded by <a href="https://www.linkedin.com/in/sarah-kennedy-02b2b6293/" target="_blank" rel="noopener noreferrer">Sarah Kennedy</a> and is <a href="https://github.com/SKennedy4123/kennedy-react-weather-app" target="_blank" rel="noopener noreferrer">open-sourced on GitHub.</a>
+<footer> This project was coded by <a href="https://www.linkedin.com/in/sarah-kennedy-02b2b6293/" target="_blank" rel="noopener noreferrer">Sarah Kennedy</a> and is <a href="https://github.com/SKennedy4123/kennedy-react-weather-app" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a> and <a href="https://kennedy-react-weather-app.netlify.app/?" rel="noopener noreferrer">hosted on Netlify.</a>
    </footer>
     </div>
   );
---
+++ src/WeatherForecast.js
@@ -0,0 +1,31 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+  }
+
+  let apiKey = "c119ffef35b7245a5e03b6e5724ae961";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col">
+                    <div className="WeatherForecast-day">Thu</div>
+                <WeatherIcon code="01d" size={36}/>
+                <div className="WeatherForecast-temperatures">
+                <span className="WeatherForecast-temperature-max">19°</span>
+                <span className="WeatherForecast-temperature-min">10°</span>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
