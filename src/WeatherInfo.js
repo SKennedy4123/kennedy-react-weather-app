0e4130a5a0155c4be156bcf3caf6609cbcf2c968
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-13 00:08:06 -0400

    added fahrenheit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (<div className="WeatherInfo">
@@ -10,22 +11,24 @@ export default function WeatherInfo(props) {
             <li className="text-capitalize">{props.data.description}</li>
         </ul>
         <div className="row mt-3">
-            <div className="col-6">
-                <div className="clearfix">
-                    <WeatherIcon code={props.data.icon} />
-                    <span className="float-left">
-                    <span className="temperature">{Math.round(props.data.temperature)}</span>
-                    <span className="unit">°C</span>
-                             </span>
-                             </div> 
-                </div>
-                <div className="col-6">
-                <ul>
-                    <li>Humidity: {Math.round(props.data.humidity)}%</li>
-                    <li>Wind: {Math.round(props.data.wind)} km/h</li>
-                </ul>
-                </div>
-     </div>
-     </div>
-     );
-     }
+        <div className="col-6">
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
